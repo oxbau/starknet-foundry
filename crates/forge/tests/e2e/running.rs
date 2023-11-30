52f6f2515fa18ca5b76cac02b608eba59f74703c
Artur Michalek <artur.michalek@swmansion.com>
2023-11-30 15:50:19 +0100

    Use `2023_10` cairo edition in tests

--- crates/forge/tests/e2e/running.rs
+++ crates/forge/tests/e2e/running.rs
@@ -744,7 +744,7 @@ fn init_new_project_test() {
             [package]
             name = "test_name"
             version = "0.1.0"
-            edition = "2023_11"
+            edition = "2023_10"
 
             # See more keys and their definitions at https://docs.swmansion.com/scarb/docs/reference/manifest.html
 
